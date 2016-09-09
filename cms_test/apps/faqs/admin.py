37c78706cc3d0be720e618817bf6382b58cf9087
Dan Gamble <dangamble89@gmail.com>
2016-09-09 11:41:30 +0100

    Update faqs app

---
+++ cms_test/apps/faqs/admin.py
@@ -0,0 +1,38 @@
+from cms.admin import PageBaseAdmin, SearchMetaBaseAdmin
+from django.contrib import admin
+
+from .models import Category, Faq
+
+
+@admin.register(Faq)
+class FaqAdmin(SearchMetaBaseAdmin):
+    """ Admin settings for the Faq model """
+    prepopulated_fields = {"url_title": ("question",)}
+    filter_horizontal = ("categories",)
+
+    fieldsets = (
+        (None, {
+            "fields": ["page", "question", "url_title", "answer", "categories", "order"]
+        }),
+        SearchMetaBaseAdmin.PUBLICATION_FIELDS,
+        SearchMetaBaseAdmin.SEO_FIELDS,
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(PageBaseAdmin):
+    """ Admin settings for the FAQ Category model. """
+
+    prepopulated_fields = {
+        "slug": ("title",)
+    }
+
+    fieldsets = (
+        PageBaseAdmin.TITLE_FIELDS,
+        ("Content", {
+            "fields": ("content_primary",),
+        }),
+        PageBaseAdmin.PUBLICATION_FIELDS,
+        PageBaseAdmin.NAVIGATION_FIELDS,
+        PageBaseAdmin.SEO_FIELDS,
+    )
---
+++ cms_test/apps/faqs/models.py
@@ -0,0 +1,88 @@
+import watson
+from cms.apps.pages.models import ContentBase
+from cms.models import HtmlField, PageBase, SearchMetaBase
+from django.db import models
+
+
+class Faqs(ContentBase):
+    """ A base for Faq's """
+
+    # The heading that the admin places this content under.
+    classifier = "apps"
+
+    # The urlconf used to power this content's views.
+    urlconf = "{{ project_name }}.apps.faqs.urls"
+
+    standfirst = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    per_page = models.IntegerField(
+        "faqs per page",
+        default=5,
+        blank=True,
+        null=True
+    )
+
+    def __unicode__(self):
+        return self.__str__()
+
+
+class Category(PageBase):
+    """ A category for Faq's """
+
+    content_primary = HtmlField(
+        "primary content",
+        blank=True
+    )
+
+    def __unicode__(self):
+        return self.__str__()
+
+    class Meta:
+        verbose_name = "catgeory"
+        verbose_name_plural = "categories"
+
+
+class Faq(SearchMetaBase):
+    """ An FAQ """
+    page = models.ForeignKey(
+        Faqs
+    )
+
+    question = models.CharField(
+        max_length=256
+    )
+
+    answer = HtmlField()
+
+    categories = models.ManyToManyField(
+        Category,
+        blank=True,
+        null=True
+    )
+
+    url_title = models.CharField(
+        max_length=256,
+        unique=True
+    )
+
+    order = models.PositiveIntegerField(
+        default=0
+    )
+
+    def __unicode__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = "FAQ"
+        verbose_name_plural = "FAQs"
+        ordering = ['order', 'id', 'question']
+
+    def get_absolute_url(self):
+        return self.page.page.reverse('faq', kwargs={
+            'faq_title': self.url_title,
+        })
+
+watson.register(Faq)
